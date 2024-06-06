cce2679b7f689c79476577a1ffffc75a4b487b1a
rafia-khan <khanrafia9218@gmail.com>
2024-06-07 01:44:41 +0600

    first commit

---
+++ evenlisener JQuery/even.js
@@ -0,0 +1,3 @@
+$("button").click(function(){
+    $("h1").text("you have click the button")
+})
---
+++ log in form project/log_in.js
@@ -0,0 +1,18 @@
+$("#loginButton").click(function(){
+
+var password1 =$("pass1").val();
+var password2 = $("pass2").val();
+
+if(password1!="" & password2!=""){
+
+}else{
+    alert("please enter password")
+}
+
+
+
+
+})
+
+$("loginButton").click
+
